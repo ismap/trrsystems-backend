263308226166b8a98a18af52712b19dbc3768534
Ismael Jimenez Peña <ipea@trr.local>
2023-04-18 11:46:57 -0600

    change

--- trrsystems/settings.py
+++ trrsystems/settings.py
@@ -50,7 +50,6 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
-    'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 5
 }
